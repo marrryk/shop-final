2dd482e35b7acce6092dec32502da3f872e9a622
marrryk <pitsenkomary@gmail.com>
2017-09-02 14:13:33 +0300

    added filters and style for them
    Still need to check fonts in all document.

--- js/items.js
+++ js/items.js
@@ -5,3 +5,16 @@ function changeImage(event) {
 	document.getElementById("mainImage").src = targetElement.getAttribute("src");
 	}
 }
+
+
+
+var countItems = 0;
+var money = 0;
+function addToCart() {
+	var increaseItem = document.body.querySelectorAll("#count");
+	var cart = document.body.querySelector(".money");
+	money += Math.floor(Math.random()*(400-250)+250);
+	countItems++;
+	increaseItem[0].innerText = countItems;
+	cart.innerText = 'Bag' + money + '';
+}
--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,32 @@
-var menuBtn = document.getElementById('menuBtn');
-var isOpenMenu = false;
-menuBtn.addEventListener('click', function () {
-  var menu = document.getElementsByClassName('mobile-group')[0];
-  if (isOpenMenu) {
-    menu.className = 'mobile-group';
-    menuBtn.className = menuBtn.className.split(' ')[0] + ' menu-btn-closed';
-  } else {
-    menu.className += ' active-menu';
-    menuBtn.className = menuBtn.className.split(' ')[0] + ' menu-btn-opened'
-  }
-  isOpenMenu = !isOpenMenu;
-});
+window.addEventListener('load', () => {
+  
+    let isSearchFieldInputOpened = false;
+    let isOpenMenu = false;
+    let menuBtn = document.getElementById('menuBtn');
+  
+    menuBtn.addEventListener('click', () => {
+      let menu = document.getElementsByClassName('mobile-visible-menu')[0];
+      if (isOpenMenu) {
+        menu.className = 'mobile-visible-menu';
+        menuBtn.className = menuBtn.className.split(' ')[0] + ' menu-btn-closed';
+      } else {
+        menu.className += ' active-menu';
+        menuBtn.className = menuBtn.className.split(' ')[0] + ' menu-btn-opened'
+      }
+      isOpenMenu = !isOpenMenu;
+    });
+  
+  
+    let magnifier = document.querySelector('.search-icon');
+    magnifier.addEventListener('click', () => {
+      let searchFieldInput = document.querySelector('.search input');
+      // console.log(searchFieldInput.classList);
+      if (isSearchFieldInputOpened) {
+        searchFieldInput.className = '';
+      } else {
+        searchFieldInput.className = 'search-input-field';
+      }
+  
+      isSearchFieldInputOpened = !isSearchFieldInputOpened;
+    });
+  });
